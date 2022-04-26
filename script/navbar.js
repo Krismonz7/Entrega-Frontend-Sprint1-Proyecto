c013b108ae558e2ac4554dae85ad55958a75ea78
krismonz7 <kriz.monz707@gmail.com>
2022-04-26 16:01:44 -0500

    Termino seccion bienvenida - index.html , styles.css

--- script/navbar.js
+++ script/navbar.js
@@ -9,41 +9,4 @@ ham.addEventListener('click', () => {
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
     
-
-localStorage.setItem('productos',[
-    {
-        "nombre":"Limon",
-        "precio":"26,8",
-        "descuento":"32",
-        "imagen":"img/ofertas/Product.jpg"
-
-    },
-    {
-        "nombre":"Tomate",
-        "precio":"21",
-        "descuento":"20",
-        "imagen":"img/ofertas/Product (1).jpg"
-
-    },
-    {
-        "nombre":"Aguacate",
-        "precio":"30",
-        "descuento":"24",
-        "imagen":"img/ofertas/Product.png"
-    },
-    {
-        "nombre":"manzana",
-        "precio":"17",
-        "descuento":"10",
-        "imagen":"img/ofertas/Product (2).png"
-
-    },
-    {
-        "nombre":"Hoja de cactus",
-        "precio":"10",
-        "descuento":"57",
-        "imagen":"img/ofertas/Product (1).png"
-
-    }
-])
 });
