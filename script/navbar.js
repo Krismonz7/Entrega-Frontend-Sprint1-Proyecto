c6252c0dd68330927ee926907d62857ae50b18e8
krismonz7 <kriz.monz707@gmail.com>
2022-04-26 12:06:20 -0500

    Leve austes en el tamaño de los botones de la ventana modal - modalUbicacion.css

--- script/navbar.js
+++ script/navbar.js
@@ -8,42 +8,42 @@ ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
-});
-
+    
 
 localStorage.setItem('productos',[
-        {
-            "nombre":"Limon",
-            "precio":"26,8",
-            "descuento":"32",
-            "imagen":"img/ofertas/Product.jpg"
+    {
+        "nombre":"Limon",
+        "precio":"26,8",
+        "descuento":"32",
+        "imagen":"img/ofertas/Product.jpg"
 
-        },
-        {
-            "nombre":"Tomate",
-            "precio":"21",
-            "descuento":"20",
-            "imagen":"img/ofertas/Product (1).jpg"
+    },
+    {
+        "nombre":"Tomate",
+        "precio":"21",
+        "descuento":"20",
+        "imagen":"img/ofertas/Product (1).jpg"
 
-        },
-        {
-            "nombre":"Aguacate",
-            "precio":"30",
-            "descuento":"24",
-            "imagen":"img/ofertas/Product.png"
-        },
-        {
-            "nombre":"manzana",
-            "precio":"17",
-            "descuento":"10",
-            "imagen":"img/ofertas/Product (2).png"
+    },
+    {
+        "nombre":"Aguacate",
+        "precio":"30",
+        "descuento":"24",
+        "imagen":"img/ofertas/Product.png"
+    },
+    {
+        "nombre":"manzana",
+        "precio":"17",
+        "descuento":"10",
+        "imagen":"img/ofertas/Product (2).png"
 
-        },
-        {
-            "nombre":"Hoja de cactus",
-            "precio":"10",
-            "descuento":"57",
-            "imagen":"img/ofertas/Product (1).png"
+    },
+    {
+        "nombre":"Hoja de cactus",
+        "precio":"10",
+        "descuento":"57",
+        "imagen":"img/ofertas/Product (1).png"
 
-        }
-    ])
+    }
+])
+});
