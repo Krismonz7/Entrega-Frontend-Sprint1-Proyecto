a3386b39cfb49329eef4b4cc7db6a56cebfd7d31
krismonz7 <kriz.monz707@gmail.com>
2022-04-28 13:49:57 -0500

    Leves cambios en los estilos de la ventana modal - modalDetalle.css

--- script/modalDetalle.js
+++ script/modalDetalle.js
@@ -1,15 +1,14 @@
 const open2 =  document.getElementById('open2');
+const detalle_container =  document.getElementById('detalle_container');
+const modal_detalle = document.getElementById('modal_detalle');
 const close2 = document.getElementById('close2');
 
-const detalle_container =  document.getElementById('detalle_container');
-const modal_detalle = document.getElementById('modal_detalle')
-
 open2.addEventListener('click',()=>{
-    detalle_container.classList.add('show');
-    modal_detalle.classList.add('show');
+    detalle_container.classList.add('show2');
+    modal_detalle.classList.add('show2');
 })
 
 close2.addEventListener('click',()=>{
-    detalle_container.classList.remove('show');
-    modal_detalle.classList.remove('show');
+    detalle_container.classList.remove('show2');
+    modal_detalle.classList.remove('show2');
 })
